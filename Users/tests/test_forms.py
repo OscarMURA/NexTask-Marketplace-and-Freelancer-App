04868879057ca668a56559ae8ace2188d5ef2e5d
diegopolancolozano <diegoarmandopolancolozano@gmail.com>
2024-09-18 16:37:40 -0500

    feat: added more test for the forms

--- Users/tests/test_forms.py
+++ Users/tests/test_forms.py
@@ -195,3 +195,137 @@ def test_portfolio_formset_valid(freelancer_profile):
     formset = PortfolioFormSet(data, instance=freelancer_profile)
 
     assert formset.is_valid()
+
+
+
+
+##########
+#Not valid cases
+##########
+
+
+
+@pytest.mark.django_db
+def test_freelancer_signup_form_invalid_missing_username():
+    # Datos simulados sin el campo requerido 'username'
+    data = {
+        'email': 'testfreelancer@example.com',
+        'first_name': 'Test',
+        'last_name': 'Freelancer',
+        'password1': 'testpassword123',
+        'password2': 'testpassword123',
+        'country': 'US',
+        'city': 'New York',
+        'phone': '1234567890',
+        'address': '1234 Freelance Ave'
+    }
+
+    form = FreelancerSignUpForm(data=data)
+    
+    # Verificar que el formulario no sea válido debido a la falta de 'username'
+    assert not form.is_valid()
+    assert 'username' in form.errors
+
+
+@pytest.mark.django_db
+def test_freelancer_signup_form_invalid_email_format():
+    # Datos simulados con un formato de correo inválido
+    data = {
+        'username': 'testfreelancer',
+        'email': 'invalidemail',
+        'first_name': 'Test',
+        'last_name': 'Freelancer',
+        'password1': 'testpassword123',
+        'password2': 'testpassword123',
+        'country': 'US',
+        'city': 'New York',
+        'phone': '1234567890',
+        'address': '1234 Freelance Ave'
+    }
+
+    form = FreelancerSignUpForm(data=data)
+    
+    # Verificar que el formulario no sea válido debido al formato incorrecto del correo
+    assert not form.is_valid()
+    assert 'email' in form.errors
+
+@pytest.mark.django_db
+def test_freelancer_signup_form_invalid_short_password():
+    # Datos simulados con una contraseña demasiado corta
+    data = {
+        'username': 'testfreelancer',
+        'email': 'testfreelancer@example.com',
+        'first_name': 'Test',
+        'last_name': 'Freelancer',
+        'password1': 'short',
+        'password2': 'short',
+        'country': 'US',
+        'city': 'New York',
+        'phone': '1234567890',
+        'address': '1234 Freelance Ave'
+    }
+
+    form = FreelancerSignUpForm(data=data)
+    
+    # Verificar que el formulario no sea válido debido a la contraseña demasiado corta
+    assert not form.is_valid()
+    assert 'password2' in form.errors
+
+@pytest.mark.django_db
+def test_client_signup_form_invalid_website_format():
+    # Datos simulados con una URL de empresa no válida
+    data = {
+        'username': 'testclient',
+        'email': 'testclient@example.com',
+        'first_name': 'Test',
+        'last_name': 'Client',
+        'password1': 'testpassword123',
+        'password2': 'testpassword123',
+        'country': 'US',
+        'city': 'New York',
+        'phone': '1234567890',
+        'address': '1234 Client Ave',
+        'company_name': 'Test Company',
+        'company_website': 'invalidurl'
+    }
+
+    form = ClientSignUpForm(data=data)
+    
+    # Verificar que el formulario no sea válido debido al formato incorrecto de la URL
+    assert not form.is_valid()
+    assert 'company_website' in form.errors
+
+def test_work_experience_formset_missing_required_fields():
+    data = {
+        'form-TOTAL_FORMS': '1',
+        'form-INITIAL_FORMS': '0',
+        'form-0-start_date': '2024-09-10',
+        'form-0-end_date': '2024-09-20',
+        'form-0-company': '',  # Campo obligatorio vacío
+        'form-0-position': '',  # Campo obligatorio vacío
+    }
+    formset = WorkExperienceFormSet(data)
+    assert not formset.is_valid()
+    assert 'company' in formset.forms[0].errors  # Verificar que hay un error en el campo `company`
+    assert 'position' in formset.forms[0].errors  # Verificar que hay un error en el campo `position`
+
+@pytest.mark.django_db
+def test_work_experience_formset_missing_required_fields(freelancer_profile):
+    # Datos simulados faltando campos requeridos
+    data = {
+        'workexperience_set-TOTAL_FORMS': '1',
+        'workexperience_set-INITIAL_FORMS': '0',
+        'workexperience_set-MIN_NUM_FORMS': '0',
+        'workexperience_set-MAX_NUM_FORMS': '1000',
+        'workexperience_set-0-company_name': '',  # Campo requerido faltante
+        'workexperience_set-0-position': 'Developer',
+        'workexperience_set-0-start_date': '2020-01-01',
+        'workexperience_set-0-end_date': '2021-01-01',
+        'workexperience_set-0-description': 'Developed cool stuff'
+    }
+
+    formset = WorkExperienceFormSet(data, instance=freelancer_profile)
+
+    # Verificar que el formulario no sea válido debido a los campos faltantes
+    assert not formset.is_valid()
+    assert 'company_name' in formset.forms[0].errors  # Verifica que hay un error en el campo 'company_name'
--- Users/tests/test_views.py
+++ Users/tests/test_views.py
@@ -3,6 +3,7 @@ import pytest
 from django.urls import reverse
 from django.test import Client
 from django.contrib.auth import get_user_model
+from Users.forms import WorkExperienceFormSet
 from Users.models import FreelancerProfile
 
 User = get_user_model()
@@ -133,3 +134,97 @@ def test_freelancer_profile_creation_invalid_data(client, user):
     # Verificar que la respuesta es la página del formulario con errores
     assert response.status_code == 200
     assert 'Este campo no puede estar vacío' in response.content.decode()
+
+def test_work_experience_formset_missing_dates():
+    data = {
+        'form-TOTAL_FORMS': '1',
+        'form-INITIAL_FORMS': '0',
+        'form-0-start_date': '',  # Fecha de inicio vacía
+        'form-0-end_date': '',  # Fecha de fin vacía
+        'form-0-company': 'Empresa XYZ',
+        'form-0-position': 'Desarrollador',
+    }
+    formset = WorkExperienceFormSet(data)
+    assert not formset.is_valid()
+    assert 'start_date' in formset.forms[0].errors  # Verificar que hay un error en `start_date`
+    assert 'end_date' in formset.forms[0].errors  # Verificar que hay un error en `end_date`
+
+def test_work_experience_formset_invalid_position_characters():
+    data = {
+        'form-TOTAL_FORMS': '1',
+        'form-INITIAL_FORMS': '0',
+        'form-0-start_date': '2024-09-10',
+        'form-0-end_date': '2024-09-20',
+        'form-0-company': 'Empresa XYZ',
+        'form-0-position': 'Desarrollador@#$',  # Caracteres especiales no permitidos
+    }
+    formset = WorkExperienceFormSet(data)
+    assert not formset.is_valid()
+    assert 'position' in formset.forms[0].errors  # Verificar que hay un error en `position`
+
+def test_work_experience_formset_text_instead_of_dates():
+    data = {
+        'form-TOTAL_FORMS': '1',
+        'form-INITIAL_FORMS': '0',
+        'form-0-start_date': 'diez de septiembre de 2024',  # Formato incorrecto
+        'form-0-end_date': 'veinte de septiembre de 2024',  # Formato incorrecto
+        'form-0-company': 'Empresa XYZ',
+        'form-0-position': 'Desarrollador',
+    }
+    formset = WorkExperienceFormSet(data)
+    assert not formset.is_valid()
+    assert 'start_date' in formset.forms[0].errors  # Verificar que hay un error en `start_date`
+    assert 'end_date' in formset.forms[0].errors  # Verificar que hay un error en `end_date`
+
+def test_work_experience_formset_company_name_too_long():
+    data = {
+        'form-TOTAL_FORMS': '1',
+        'form-INITIAL_FORMS': '0',
+        'form-0-start_date': '2024-09-10',
+        'form-0-end_date': '2024-09-20',
+        'form-0-company': 'X' * 256,  # Límite de longitud excedido (ej. 255 caracteres)
+        'form-0-position': 'Desarrollador',
+    }
+    formset = WorkExperienceFormSet(data)
+    assert not formset.is_valid()
+    assert 'company' in formset.forms[0].errors  # Verificar que hay un error en `company`
+
+def test_work_experience_formset_missing_end_date_for_non_current_job():
+    data = {
+        'form-TOTAL_FORMS': '1',
+        'form-INITIAL_FORMS': '0',
+        'form-0-start_date': '2024-09-10',
+        'form-0-end_date': '',  # Faltante, pero no está marcado como trabajo actual
+        'form-0-company': 'Empresa XYZ',
+        'form-0-position': 'Desarrollador',
+    }
+    formset = WorkExperienceFormSet(data)
+    assert not formset.is_valid()
+    assert 'end_date' in formset.forms[0].errors  # Verificar que hay un error por falta de `end_date`
+
+def test_work_experience_formset_dates_out_of_range():
+    data = {
+        'form-TOTAL_FORMS': '1',
+        'form-INITIAL_FORMS': '0',
+        'form-0-start_date': '1900-01-01',  # Fecha fuera del rango permitido
+        'form-0-end_date': '1900-12-31',  # Fecha fuera del rango permitido
+        'form-0-company': 'Empresa XYZ',
+        'form-0-position': 'Desarrollador',
+    }
+    formset = WorkExperienceFormSet(data)
+    assert not formset.is_valid()
+    assert 'start_date' in formset.forms[0].errors  # Verificar que hay un error en `start_date`
+
+def test_work_experience_formset_missing_both_dates_for_current_job():
+    data = {
+        'form-TOTAL_FORMS': '1',
+        'form-INITIAL_FORMS': '0',
+        'form-0-start_date': '',  # Fecha faltante
+        'form-0-end_date': '',  # Fecha faltante
+        'form-0-company': 'Empresa XYZ',
+        'form-0-position': 'Desarrollador',
+        'form-0-current_job': 'on',  # Marcado como trabajo actual
+    }
+    formset = WorkExperienceFormSet(data)
+    assert not formset.is_valid()
+    assert 'start_date' in formset.forms[0].errors  # Verificar que hay un error en `start_date`
