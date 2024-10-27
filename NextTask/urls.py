ad8e80911e485b383fb7db658a0a3f1fc8cb979b
OscarMURA <1083870123@u.icesi.edu.co>
2024-10-27 07:56:22 -0500

    fix: Errors about the templates

--- NextTask/urls.py
+++ NextTask/urls.py
@@ -6,7 +6,6 @@ from django.conf.urls.static import static  # Para servir archivos estáticos y 
 from . import views
 from django.conf.urls.i18n import i18n_patterns
 
-
 # Rutas que no deben tener prefijo de idioma (como las APIs)
 urlpatterns = [path('messaging/', include('Messaging.urls', namespace='messaging')),]  # Incluye las rutas de la app Messaging sin i18n]
 
