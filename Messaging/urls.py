5280c509f3e706cf2fe8ff73f7a9dfe52b68e8c9
OscarMURA <1083870123@u.icesi.edu.co>
2024-11-08 17:33:31 -0500

    feat: changed parts of messaging routes that caused errors, improved design and buttons. Also some necessary translations were added

--- Messaging/urls.py
+++ Messaging/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 
-app_name = 'messaging'
 
 urlpatterns = [
     path('freelancer/', views.freelancer_chat, name='freelancer_chat'),
--- NextTask/urls.py
+++ NextTask/urls.py
@@ -7,7 +7,7 @@ from . import views
 from django.conf.urls.i18n import i18n_patterns
 
 # Rutas que no deben tener prefijo de idioma (como las APIs)
-urlpatterns = [path('messaging/', include('Messaging.urls', namespace='messaging')),]  # Incluye las rutas de la app Messaging sin i18n]
+urlpatterns = [path('messaging/', include('Messaging.urls')),]  # Incluye las rutas de la app Messaging sin i18n]
 
 # Rutas con soporte de internacionalización
 urlpatterns += i18n_patterns(
